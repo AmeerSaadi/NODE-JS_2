0369ce86eb24b840de8dfbd1275b8b4eeb08c714
AmeerSaadi <ameersaadi77@gmail.com>
2025-03-06 19:28:03 +0200

    Update index.js
    הגדרת תיקיית public כסטטית והוספת ראוטרים לניהול משתמשים ומדידות

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ global.db_pool = db_M.pool;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const users_R = require('./Routers/users_R');
+const measurements_R = require('./Routers/measurements_R');
+app.use('/users', users_R);
+app.use('/measurements', measurements_R);
