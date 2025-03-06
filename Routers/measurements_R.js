aba202b8e3bd8b85b666c667a6b67a6ad78613d3
AmeerSaadi <ameersaadi77@gmail.com>
2025-03-06 19:45:22 +0200

    Update measurements_R.js
    הוספת נתיבים עבור מדידות והיסטוריית מדידות עם Middleware מתאים

--- Routers/measurements_R.js
+++ Routers/measurements_R.js
@@ -2,3 +2,19 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const measurements_Mid = require('../Middleware/measurements_Mid');
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'measurements.html'));
+});
+
+
+router.post('/', measurements_Mid.addMeasurement, (req, res) => {
+    res.status(200).json({ message: 'Measurement added successfully' });
+});
+
+
+router.get('/history/:userId', measurements_Mid.getHistory, (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'history.html'));
+});
+
+module.exports = router;
