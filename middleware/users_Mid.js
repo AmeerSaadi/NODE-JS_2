76b76114d68516ac48d4c4d08e0d94e0f1fe4f96
AmeerSaadi <ameersaadi77@gmail.com>
2025-03-06 20:01:41 +0200

    Update users_Mid.js
    הוספת Middleware להוספת משתמשים ולקבלת רשימת משתמשים

--- middleware/users_Mid.js
+++ middleware/users_Mid.js
@@ -9,3 +9,19 @@ async function addUser(req, res, next) {
         res.status(500).json({ message: 'Error adding user' });
     }
 }
+
+async function getUsers(req, res, next) {
+    const query = 'SELECT * FROM users';
+    try {
+        const [rows] = await db_pool.promise().query(query);
+        req.users = rows;
+        next();
+    } catch (err) {
+        res.status(500).json({ message: 'Error fetching users' });
+    }
+}
+
+module.exports = {
+    addUser,
+    getUsers
+};
